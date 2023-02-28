4a0d2d90d3ef8ce4de295b391f8b7bdf9836212c
Emily0330 <99779672+Emily0330@users.noreply.github.com>
2023-02-28 17:17:56 +0800

    connecting with smart contract -1
    use infura as a web3Provider (still does not work, trying to solve)

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 const express = require('express')
 const app = express()
+const Web3 = require("web3");
 const port = 3000
+const fs = require('fs');//讀檔案用(?)
+const path = require('path');
+const bodyParser = require("body-parser");//parse the body as json-formatted
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-//post request
-/*
-const { exec } = require('child_process');
 
-app.post('/example', function(req, res) {
-  exec('node ./path/to/your/token-minting-program.js', (error, stdout, stderr) => {
-    if (error) {
-      console.error(exec error: ${error});
-      return;
-    }
-    console.log(stdout: ${stdout});
-    console.error(stderr: ${stderr});
-  });
-});
-*/
-//const bodyParser = require('body-parser');
-//app.use(bodyParser.json());
+//const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); //後端的URL
+const web3 = new Web3('https://goerli.infura.io/v3/f1c999036e8b4d2b93e5984c7e0ef4f4');
+const TOKEN_CONTRACT_ADDRESS = "0xD1723BDB1bd1a6e75f84e08444bdf107Cf9c90E0";
+const abiFile = fs.readFileSync(path.join(__dirname, 'abi', 'SmartConABI.json'));
+const abiFilePath = path.join(__dirname, 'abi', 'SmartConABI.json');
+
+const ABI = JSON.parse(abiFile);//告訴program要用json格式來看abi
+
 app.get('/api',(req,res)=>{
     res.send('API Page')
 })
-
+// add middleware to parse request body
+app.use(bodyParser.json());
 // endpoint for minting a token (requires authentication)
-app.use(express.urlencoded({ extended: true }))//是在設定接受的請求格式(?)用express.urlencoded()中介軟體來解析URL編碼的請求body
-app.post('/api', (req, res) => { //app.post("/mint", requireAuth, async (req, res) => {
+//app.use(express.urlencoded({ extended: true }))//是在設定接受的請求格式(?)用express.urlencoded()中介軟體來解析URL編碼的請求body
+app.post('/api', async (req, res) => { //app.post("/mint", requireAuth, async (req, res) => {
 	try {
-        //res.send('success!-1');
-        console.log(req.body); // 輸出 { name: 'John', age: 30 }
-        res.send('已接收到資料')
-        console.log('success!!');//test
-		//const { to, amount } = req.body; // get the "to" address and "amount" from the request body
-		//const contract = new web3.eth.Contract(ABI, TOKEN_CONTRACT_ADDRESS); // create a contract instance using your token contract ABI and address
-		//const result = await contract.methods
-			//.mint(to, amount)
-			//.send({ from: web3.eth.defaultAccount }); // use the contract method to mint the token
-		/*res.status(200).send(
-			//`Token minted with transaction hash: ${result.transactionHash}`
-            `Token minted with transaction hash: success!!`//for testing
-		);*/
+    console.log("success!"); // 輸出 { name: 'John', age: 30 }
+    res.send('已接收到資料')
+    //大組長傳的那段
+		const { to, amount } = req.body; // get the "to" address and "amount" from the request body
+    console.log(req.body.to);//testing
+		const contract = new web3.eth.Contract(ABI, TOKEN_CONTRACT_ADDRESS); // create a contract instance using your token contract ABI and address
+		const result = await contract.methods
+			.mint(to, amount)
+			.send({ from: "0xbe804512d5128516cD95b0c6610a7881cfDdD062" }); // use the contract method to mint the token //mint tokens from this account
+		res.status(200).send(
+			`Token minted with transaction hash: ${result.transactionHash}`
+      //`Token minted with transaction hash: success!!`//for testing
+		);
 	} catch (err) {
 		console.error(err);
 		res.status(500).send("Error while minting token");
