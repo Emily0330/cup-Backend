5281e2f1afceb19a0c1cc73ab03ca43cc3f518af
Emily0330 <99779672+Emily0330@users.noreply.github.com>
2023-03-05 23:04:13 +0800

    setting gasLimit
    not yet done: 檢查to是地址/amount是數字(if,try,catch)throw error

--- index.js
+++ index.js
@@ -26,27 +26,25 @@ app.get('/api',(req,res)=>{
 })
 // add middleware to parse request body
 app.use(bodyParser.json());
-// endpoint for minting a token (requires authentication)
-//app.use(express.urlencoded({ extended: true }))//是在設定接受的請求格式(?)用express.urlencoded()中介軟體來解析URL編碼的請求body
-//已確認節點有在監聽JSON-RPC API請求
-
+const contract = new web3.eth.Contract(ABI, TOKEN_CONTRACT_ADDRESS);// create a contract instance using your token contract ABI and address
+//above are for testing
 app.post('/api', async (req, res) => { //app.post("/mint", requireAuth, async (req, res) => {
 	try {
-    console.log("success!"); // 輸出 { name: 'John', age: 30 }
-    res.send('已接收到資料')
-    //大組長傳的那段
-		// this is one of the ways to sign a transaction on your end
+    	console.log("success!"); // 輸出 { name: 'John', age: 30 }
+    	//res.send('已接收到資料')
+    	//大組長傳的那段
+		// this is one of the other ways to sign a transaction on your end
 		web3.eth.accounts.wallet.add("PrivateKey_of_fromAddress");
 		//above are for testing
+		//檢查to是地址/amount是數字(if,try,catch)throw error
 		const { to, amount } = req.body; // get the "to" address and "amount" from the request body
-    	console.log(req.body.to);//testing
-		const contract = new web3.eth.Contract(ABI, TOKEN_CONTRACT_ADDRESS); // create a contract instance using your token contract ABI and address
+    	console.log('請求的body:'+req.body.to);//testing
 		const result = await contract.methods
 			.mint(to, amount)
-			.send({ from: "fromAddress" }); // use the contract method to mint the token //mint tokens from this account
+			.send({ from: "fromAddress" , gas: 1000000}); // use the contract method to mint the token //mint tokens from this account
 		res.status(200).send(
 			`Token minted with transaction hash: ${result.transactionHash}`
-      //`Token minted with transaction hash: success!!`//for testing
+      			//`Token minted with transaction hash: success!!`//for testing
 		);
 	} catch (err) {
 		console.error(err);
